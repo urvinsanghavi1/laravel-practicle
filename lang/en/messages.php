f22094697dca08309d1c3424bd61de18d1a6cd7b
unknown <urvin.sanghavi@viitor.cloud>
2023-01-25 15:06:16 +0530

    env change with lang added

---
+++ lang/en/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Custome Messages for User
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the messages string which will show
+    | all the user of the system
+    */
+
+    "auth_wrong" => "Email Address and Password Wrong.",
+    "auth_success" => "Login Successfully.",
+    "company_login_success" => "Company Registred Successfully. Please Check Your Email !",
+    "profile_update_success" => "Update Profile Successfully",
+
+    "country_error" => "Please Select Country",
+    "state_error" => "Please Select State and Country",
+    "password_validation" => "Password must contain 1 capital letter, 1 small letter, and 1 special characters.",
+    "exist" => "Company Name already Exist.",
+];
